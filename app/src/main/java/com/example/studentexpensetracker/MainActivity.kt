7d461651a15574c4280930f099377c6c666043c8
Your Name <youremail@example.com>
2025-02-09 02:56:15 +0530

    1st commit

---
+++ app/src/main/java/com/example/studentexpensetracker/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.studentexpensetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.studentexpensetracker.ui.theme.StudentExpenseTrackerTheme
+import com.google.firebase.FirebaseApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this) // Initialize Firebase
+        enableEdgeToEdge()
+        setContent {
+            StudentExpenseTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StudentExpenseTrackerTheme {
+        Greeting("Android") //hello test message
+    }
+}
